a3fa2f4729ae767f20fc03a4acc714d644ca78b8
Fourkanul Islam Mobin <fourkan246@gmail.com>
2018-08-30 00:53:01 +0600

    Initial commit

---
+++ app/src/main/java/com/example/mobin/visualassistant/MainActivity.java
@@ -0,0 +1,58 @@
+package com.example.mobin.visualassistant;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    Button btnDetect, btnRead, btnFind, btnEmergency;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        btnDetect=findViewById(R.id.idDetect);
+        btnRead=findViewById(R.id.idRead);
+        btnFind=findViewById(R.id.idFind);
+        btnEmergency=findViewById(R.id.idEmergency);
+
+
+        btnDetect.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), DetectObject.class);
+                startActivity(i);
+            }
+        });
+
+        btnRead.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Ocr.class);
+                startActivity(i);
+            }
+        });
+
+        btnFind.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), FindObject.class);
+                startActivity(i);
+            }
+        });
+
+        btnEmergency.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(), Emergency.class);
+                startActivity(i);
+            }
+        });
+
+    }
+}
