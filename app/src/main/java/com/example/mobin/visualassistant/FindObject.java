50a19eb31353dbeea4c51f372a63496f26dd6e76
Fourkanul Islam Mobin <fourkan246@gmail.com>
2018-09-28 16:33:26 +0600

    object detection library added

--- app/src/main/java/com/example/mobin/visualassistant/FindObject.java
+++ app/src/main/java/com/example/mobin/visualassistant/FindObject.java
@@ -42,7 +42,7 @@ public class FindObject extends AppCompatActivity {
             @Override
             public void onInit(int status) {
                 if (status == TextToSpeech.SUCCESS) {
-                    int result = mTTS.setLanguage(Locale.GERMAN);
+                    int result = mTTS.setLanguage(Locale.US);
 
                     if (result == TextToSpeech.LANG_MISSING_DATA
                             || result == TextToSpeech.LANG_NOT_SUPPORTED) {
